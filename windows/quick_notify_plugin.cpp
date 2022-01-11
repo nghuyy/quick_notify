9fdca83b81d298ba35d3b5d1aaeda2f9e1838d7f
Huy Nguyễn <huyndx@outlook.com>
2022-01-11 17:23:07 +0700

    .

--- windows/quick_notify_plugin.cpp
+++ windows/quick_notify_plugin.cpp
@@ -21,76 +21,80 @@ using namespace winrt;
 using namespace Windows::UI::Notifications;
 using namespace Windows::Data::Xml::Dom;
 
+
 namespace {
 
-class QuickNotifyPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+    class QuickNotifyPlugin : public flutter::Plugin {
+     public:
+      static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
 
-  QuickNotifyPlugin();
+      QuickNotifyPlugin();
 
-  virtual ~QuickNotifyPlugin();
+      virtual ~QuickNotifyPlugin();
 
- private:
-  // Called when a method is called on this plugin's channel from Dart.
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+     private:
+      // Called when a method is called on this plugin's channel from Dart.
+      void HandleMethodCall(
+          const flutter::MethodCall<flutter::EncodableValue> &method_call,
+          std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
   
-  ToastNotifier toastNotifier_{ ToastNotificationManager::CreateToastNotifier(L"quick_notify") };
-};
+      ToastNotifier toastNotifier_{ ToastNotificationManager::CreateToastNotifier(L"quick_notify") };
+    };
 
 
-// static
-void QuickNotifyPlugin::RegisterWithRegistrar(
-    flutter::PluginRegistrarWindows *registrar) {
-  auto channel =
-      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "quick_notify",
-          &flutter::StandardMethodCodec::GetInstance());
+    // static
+    void QuickNotifyPlugin::RegisterWithRegistrar(
+        flutter::PluginRegistrarWindows *registrar) {
+      auto channel =
+          std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+              registrar->messenger(), "quick_notify",
+              &flutter::StandardMethodCodec::GetInstance());
 
-  auto plugin = std::make_unique<QuickNotifyPlugin>();
+      auto plugin = std::make_unique<QuickNotifyPlugin>();
 
-  channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto &call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
+      channel->SetMethodCallHandler(
+          [plugin_pointer = plugin.get()](const auto &call, auto result) {
+            plugin_pointer->HandleMethodCall(call, std::move(result));
+          });
 
-  registrar->AddPlugin(std::move(plugin));
-}
-
-QuickNotifyPlugin::QuickNotifyPlugin() {}
-
-QuickNotifyPlugin::~QuickNotifyPlugin() {}
-
-void QuickNotifyPlugin::HandleMethodCall(
-    const flutter::MethodCall<flutter::EncodableValue> &method_call,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
+      registrar->AddPlugin(std::move(plugin));
     }
-    result->Success(flutter::EncodableValue(version_stream.str()));
-  } else if (method_call.method_name().compare("notify") == 0) {
-    auto args = std::get<flutter::EncodableMap>(*method_call.arguments());
-    auto content = std::get<std::string>(args[flutter::EncodableValue("content")]);
 
-    auto toastContent = ToastNotificationManager::GetTemplateContent(ToastTemplateType::ToastText01);
-    XmlNodeList xmlNodeList = toastContent.GetElementsByTagName(L"text");
-    xmlNodeList.Item(0).AppendChild(toastContent.CreateTextNode(winrt::to_hstring(content)));
-    ToastNotification toastNotification{ toastContent };
-    toastNotifier_.Show(toastNotification);
-    result->Success(nullptr);
-  } else {
-    result->NotImplemented();
-  }
-}
+    QuickNotifyPlugin::QuickNotifyPlugin() {}
+
+    QuickNotifyPlugin::~QuickNotifyPlugin() {}
+
+    void QuickNotifyPlugin::HandleMethodCall(
+        const flutter::MethodCall<flutter::EncodableValue> &method_call,
+        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
+        if (method_call.method_name().compare("getPlatformVersion") == 0) {
+            std::ostringstream version_stream;
+            version_stream << "Windows ";
+            if (IsWindows10OrGreater()) {
+                version_stream << "10+";
+            }
+            else if (IsWindows8OrGreater()) {
+                version_stream << "8";
+            }
+            else if (IsWindows7OrGreater()) {
+                version_stream << "7";
+            }
+            result->Success(flutter::EncodableValue(version_stream.str()));
+        }else if(){
+
+        } else if (method_call.method_name().compare("notify") == 0) {  
+        auto args = std::get<flutter::EncodableMap>(*method_call.arguments());
+        auto content = std::get<std::string>(args[flutter::EncodableValue("content")]);
+        auto toastContent = ToastNotificationManager::GetTemplateContent(ToastTemplateType::ToastText01);
+        XmlNodeList xmlNodeList = toastContent.GetElementsByTagName(L"text");
+        xmlNodeList.Item(0).AppendChild(toastContent.CreateTextNode(winrt::to_hstring(content)));
+        ToastNotification toastNotification{ toastContent };
+        toastNotifier_.Show(toastNotification);
+        result->Success(nullptr);
+      } else {
+        result->NotImplemented();
+      }
+    }
 
 }  // namespace
 
